d9cc6b74ff1a1d871f7fdf7c775b4de545478e8c
Shreyask808 <shreyask808@gmail.com>
2026-04-20 20:48:14 -0400

    function creation

--- Tire Models/Combined Brush Model/Combined_Brush_Tire_Model.m
+++ Tire Models/Combined Brush Model/Combined_Brush_Tire_Model.m
@@ -3,22 +3,22 @@ clear
 close all
 import casadi.*
 
-%% Tire Data
-Fz = SX.sym('Fz');                                                          % Nomral Force at the wheel in N
-R = SX.sym('R');                                                            % Radius of the Tire in m
-r = SX.sym('r');                                                            % Effective Radius of the Tire in m
-a = SX.sym('a');                                                            % Half the length of the contact patch in m
-w = SX.sym('w');                                                            % Width of the tire in m
-P_in = SX.sym('P_in');                                                      % Tire internal pressure in Pa
-s = SX.sym('s');                                                            % Longitudinal Slip Ratio
-alpha = SX.sym('alpha');                                                    % Slip angle of the tire in rad
+function [Fx,Fy] = combined_brsuh_model(u,v,P_in,R,w,C_p,omega,alpha,Fz,mu_0,mu)
+% Effective Radius Calculation
+a = Fz/(2*P_in*w);                                                          % Half of contact patch length (m)
+r = sqrt(R^2 - a^2);                                                        % Effective Radius of the Tire (m)
+P_max = 3*Fz/(4*a*w);
 
-% Effective Radius Estimation
-a = Fz/(2*P_in*w);                                                          % Half the contact patch length in m
-r = sqrt(R^2 - a^2);                                                        % Effective Radius of the tire in m
+% Slip Ratios
+kappa = (r*omega - u)/u;
+sigma_x = kappa/(1+kappa);
+sigma_y = tan(alpha)/(1 + kappa);
+sigma = [sigma_x; sigma_y];
 
-% Longitudinal Slip Ratio Definition
-s = (r*omega - u*cos(alpha)/(r*omega);
+% Adhesion Length
+lambda = 2*a - (C_p*norm(sigma)*a^2)/(mu_0*w*P_max);
 
-
-% Adhesion Length of the tire
+% Forces
+Fx = C_p*sigma_x*(lambda^2)/2 + (sigma_x/norm(sigma))*mu*w*P_max*((4*a/3) - (lambda^2/a) + (lambda^3/(3*a^2)));
+Fy = C_p*sigma_y*(lambda^2)/2 + (sigma_y/norm(sigma))*mu*w*P_max*((4*a/3) - (lambda^2/a) + (lambda^3/(3*a^2)));
+end
