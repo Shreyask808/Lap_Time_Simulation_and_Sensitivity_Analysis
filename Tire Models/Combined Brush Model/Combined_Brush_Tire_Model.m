b16a8464bb4b300b4e0263cd6943fedb63c8ef6f
Shreyask808 <shreyask808@gmail.com>
2026-04-21 02:08:41 -0400

    Correction to Adhesion Length

--- Tire Models/Combined Brush Model/Combined_Brush_Tire_Model.m
+++ Tire Models/Combined Brush Model/Combined_Brush_Tire_Model.m
@@ -10,15 +10,15 @@ r = sqrt(R^2 - a^2);                                                        % Ef
 P_max = 3*Fz/(4*a*w);
 
 % Slip Ratios
-kappa = (r*omega - u)/u;
+kappa = (r*omega - u)/(u + 1e-6);
 sigma_x = kappa/(1+kappa);
 sigma_y = tan(alpha)/(1 + kappa);
 sigma = [sigma_x; sigma_y];
-
+sigma_norm = sqrt(sigma_x^2 + sigma_y^2);
 % Adhesion Length
-lambda = 2*a - (C_p*norm(sigma)*a^2)/(mu_0*w*P_max);
-
+lambda = 2*a - (C_p*sigma_norm*a^2)/(mu_0*w*P_max);
+lambda = fmax(0,fmin(2*a,lambda));
 % Forces
-Fx = C_p*sigma_x*(lambda^2)/2 + (sigma_x/norm(sigma))*mu*w*P_max*((4*a/3) - (lambda^2/a) + (lambda^3/(3*a^2)));
-Fy = C_p*sigma_y*(lambda^2)/2 + (sigma_y/norm(sigma))*mu*w*P_max*((4*a/3) - (lambda^2/a) + (lambda^3/(3*a^2)));
+Fx = C_p*sigma_x*(lambda^2)/2 + (sigma_x/(sigma_norm + 1e-6))*mu*w*P_max*((4*a/3) - (lambda^2/a) + (lambda^3/(3*a^2)));
+Fy = C_p*sigma_y*(lambda^2)/2 + (sigma_y/(sigma_norm + 1e-6))*mu*w*P_max*((4*a/3) - (lambda^2/a) + (lambda^3/(3*a^2)));
 end
