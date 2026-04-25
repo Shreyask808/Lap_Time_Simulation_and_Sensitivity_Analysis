515c19e0d51d1ecef552904aae03088454ede1e2
Shreyask808 <shreyask808@gmail.com>
2026-04-24 21:11:21 -0400

    Adding Tire model, vehicle data, track data, vehicle model location

--- Optimizer/Direct_Multiple_Shooting_Optimizer.m
+++ Optimizer/Direct_Multiple_Shooting_Optimizer.m
@@ -30,4 +30,37 @@ else
     fprintf('Loaded Vehicle Data: %s\n', location_track);
 end
 
+N = 500;
+
+%% Vehicle Model Folder Selection
+[file4, loc4] = uigetfile({'*.m'}, 'Select the Tire Model.m file');
+
+if isequal(file4, 0)
+    error('No tire model file selected.');
+else
+    fprintf('Tire Model Path Added: %s\n', loc4);
+end
+
+%% Vehicle Model Folder Selection
+[file3, loc3] = uigetfile({'*.m'}, 'Select the Vehicle Model.m file');
+
+if isequal(file3, 0)
+    error('No vehicle model file selected.');
+else
+    addpath(loc3); 
+    [n_states,u_states,f_dynamics,g,lbg,ubg,lbx,ubx,x0,states,cost] = Seven_DOF_Handling_Model_2D(Car,track_data,N,loc4)
+    fprintf('Vehicle Model Path Added: %s\n', loc3);
+end
+
 %% Optimizer Definition
+nlp = struct('x', states, 'f', cost, 'g',g);
+opts = struct;
+opts.ipopt.max_iter = 5000;
+opts.ipopt.obj_scaling_factor = 1;
+opts.ipopt.nlp_scaling_method = 'gradient-based';
+solver = nlpsol('S', 'ipopt', nlp, opts);
+sol = solver('x0', x0, 'lbx', lbx, 'ubx', ubx, 'lbg', lbg, 'ubg', ubg);
+full_sol = full(sol.x);
+X_opt = reshape(full_sol(1:n_states*(N+1)), n_states, N+1);
+U_opt = reshape(full_sol(n_states*(N+1)+1:end), u_states, N);
+fprintf('Optimal Lap Time: %.3f seconds\n', X_opt(1, end)/t_scale);
