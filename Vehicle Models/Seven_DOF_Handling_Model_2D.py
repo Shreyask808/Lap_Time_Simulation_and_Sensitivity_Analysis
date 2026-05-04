aa27b473e9e0a48494aa94c802a8af6665ba60bf
Shreyask808 <shreyask808@gmail.com>
2026-05-04 19:26:50 -0400

    Update Seven_DOF_Handling_Model_2D.py

--- Vehicle Models/Seven_DOF_Handling_Model_2D.py
+++ Vehicle Models/Seven_DOF_Handling_Model_2D.py
@@ -20,13 +20,14 @@ import casadi as ca
 from casadi import *
 import matplotlib
 matplotlib.use('Qt5Agg') # Or 'TkAgg' if you don't have Qt installed
+matplotlib.use('Qt5Agg')
 import plotly.graph_objects as go
 import matplotlib.pyplot as plt
+from tire_models import get_tire_model
 os.system('cls' if os.name == 'nt' else 'clear')
 plt.close('all')
-from tire_model import combined_brush_tire_model
  
-def Seven_DOF_Handling_Model_2D(vehicle,track_data,N,loc4):
+def Seven_DOF_Handling_Model_2D(vehicle,track_data,N,name):
 #=========================================================================================================================================================================================================================
 # System Dimensions    
     n_states = 10
@@ -113,6 +114,12 @@ def Seven_DOF_Handling_Model_2D(vehicle,track_data,N,loc4):
     theta = f_theta(s)
     xi = psi - theta
 
+    # Aerodynamic Forces
+    Drag = -(1/2)*vehicle.Cd*vehicle.rho*vehicle.A*(u**2)
+    f_Drag = Function('f_Drag',{X_sym},{Drag})
+    Downforce = -(1/2)*vehicle.Cl*vehicle.rho*vehicle.A*(u**2)
+    f_Downforce = Function('f_Downforce',{X_sym},{Downforce})
+
     # Tire Contact Patch Velocities in Vehicle Frame
     # Rear Right Tire
     u_rr = u + (vehicle.Wr/2)*psi_dot
@@ -136,18 +143,75 @@ def Seven_DOF_Handling_Model_2D(vehicle,track_data,N,loc4):
     alpha_fr = -ca.arctan((-u_fr*sin(delta) + v_fr*cos(delta))/(u_fr*cos(delta) + v_fr*sin(delta)))             # Front Right Slip Angle in rad
     alpha_fl = -ca.arctan((-u_fl*sin(delta) + v_fl*cos(delta))/(u_fl*cos(delta) + v_fl*sin(delta)))             # Front Left Slip Angle in rad
 
+    # Tire Forces and Moments
+    tire_model = get_tire_model(name)
+
     # Rear Right Tire Forces
-    Fxrr, Fyrr, Mzrr, rrr = combined_brush_tire_model(u_rr,vehicle.R,vehicle.w,vehicle.Cp,vehicle.kv,O_rr,alpha_rr,Fzrr,vehicle.mu0,vehicle.mu)
+    Fxrr,Fyrr,Mzrr,rrr = tire_model(u_rr,O_rr,alpha_rr,Fzrr,vehicle)
 
     # Rear Left Tire Forces
-    Fxrl, Fyrl, Mzrl, rrl = combined_brush_tire_model(u_rl,vehicle.R,vehicle.w,vehicle.Cp,vehicle.kv,O_rl,alpha_rl,Fzrl,vehicle.mu0,vehicle.mu)
+    Fxrl,Fyrl,Mzrl,rrl = tire_model(u_rl,O_rl,alpha_rl,Fzrl,vehicle)
 
     # Front Right Tire Forces
-    Fxfr, Fyfr, Mzfr, rfr = combined_brush_tire_model(u_fr,vehicle.R,vehicle.w,vehicle.Cp,vehicle.kv,O_fr,alpha_fr,Fzfr,vehicle.mu0,vehicle.mu)
-
+    Fxfr,Fyfr,Mzfr,rfr = tire_model(u_fr,O_fr,alpha_fr,Fzfr,vehicle)
+    
     # Front Left Tire Forces
-    Fxfl, Fyfl, Mzfl, rfl = combined_brush_tire_model(u_fl,vehicle.R,vehicle.w,vehicle.Cp,vehicle.kv,O_fl,alpha_fl,Fzfl,vehicle.mu0,vehicle.mu)
+    Fxfl,Fyfl,Mzfl,rfl = tire_model(u_fl,O_fl,alpha_fl,Fzfl,vehicle)
 
-    # ODE dynamics Definition
-    s_dot = (u*cos(xi) - v*sin(xi))/(1 - n*curv)
-    n_dot = 
+    # Dynamics ODE Definition
+    s_dot = (1/length_scale)*(u*cos(xi) - v*cos(xi))/(1 - n*curv)
+    n_N_dot = (length_scale/time_scale)*(u*sin(xi) + v*cos(xi))
+    psi_N_dot = psi_dot_N
+    psi_ddot_N = (angle_scale/(time_scale**2))*(1/2)*(-2*vehicle.d*(Fyrl + Fyrr) + 2*(Mzfl + Mzfr + Mzrl + Mzrr) + (Fxrr - Fxrl)*vehicle.Wr + (2*(vehicle.d - vehicle.l)*(Fyfl + Fyfr) + vehicle.Wf*(Fxfr - Fxfl))*cos(delta) + (2*(vehicle.d - vehicle.l)*(Fxfl+Fxfr) + vehicle.Wf*(Fyfl - Fyfr))*sin(delta))*(1/vehicle.Iz)
+    u_N_dot = (speed_scale/time_scale)*(v*psi_dot + (Drag + Fxrl + Fxrr + Fxfl*cos(delta) + Fxfr*cos(delta) - Fyfl*sin(delta) - Fyfr*sin(delta))/vehicle.m)
+    v_N_dot = (speed_scale/time_scale)*(-u*psi_dot + (Fyrl + Fyrr + Fyfl*cos(delta) + Fyfr*cos(delta) + Fxfl*sin(delta) + Fxfr*sin(delta))/vehicle.m)
+    O_fl_N_dot = (angle_scale/(time_scale**2))*(Md_fl - Fxfl*rfl)*(1/vehicle.Ifl)
+    O_fr_N_dot = (angle_scale/(time_scale**2))*(Md_fr - Fxfr*rfr)*(1/vehicle.Ifr)
+    O_rl_N_dot = (angle_scale/(time_scale**2))*(Md_rl - Fxrl*rrl)*(1/vehicle.Irl)
+    O_rr_N_dot = (angle_scale/(time_scale**2))*(Md_rr - Fxrr*rrr)*(1/vehicle.Irr)
+
+    ODE = vertcat(1/s_dot, n_N_dot/s_dot, psi_N_dot/s_dot,psi_ddot_N/s_dot, u_N_dot/s_dot, v_N_dot/s_dot, O_fl_N_dot/s_dot, O_fr_N_dot/s_dot, O_rl_N_dot/s_dot, O_rr_N_dot/s_dot)
+    f_dynamics = Function('f_dynamics',[X_sym,U_sym],[ODE])
+
+# Cost Function & Constraint Definition
+    # Cost Function
+    cost = 0
+
+    # Dynamics Constraints
+    g_dynamics = []
+    lbg_dynamics = []
+    ubg_dynamics = []
+
+    # Time Constraints
+    g_time = []
+    lbg_time = []
+    ubg_time = []
+
+    # Vehicle Power Constraints
+    g_power = []
+    lbg_power = []
+    ubg_power = []
+
+    # Nonlinear Programing Definition
+    for k in range(N):
+        state = X[:,k]
+        ctrl = U[:,k]
+        # Dynamics Definition
+        # 4th Order Ranga Kutta Method for Discretization
+        k1 = f_dynamics(state,ctrl)
+        k2 = f_dynamics(state + (ds/2)*k1, ctrl)
+        k3 = f_dynamics(state + (ds/2)*k2, ctrl)
+        k4 = f_dynamics(state + (ds)*k3, ctrl)
+        state_next = state + (ds/6)*(k1 + 2*k2 + 2*k3 + k4)
+
+        # Dynamics Constraint Definition
+        g_dynamics.append(X[:,k+1] - state_next)
+        lbg_dynamics.append(np.zeros((n_states),1))
+        ubg_dynamics.append(np.zeros((n_states),1))
+
+        # Time Constraint Definition (Time is forced to move ahead)
+        g_time.append(state_next[1] - state[1])
+        lbg_time.append(1e-6)
+        ubg_time.append(np.inf)
+
+        
