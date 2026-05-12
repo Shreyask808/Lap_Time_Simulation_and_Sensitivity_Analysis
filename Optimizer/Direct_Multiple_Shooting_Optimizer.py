0fae9a95f7569780fe7e8dbaff0c94c6464ad923
Shreyask808 <shreyask808@gmail.com>
2026-05-12 01:12:00 -0400

    Update Direct_Multiple_Shooting_Optimizer.py

--- Optimizer/Direct_Multiple_Shooting_Optimizer.py
+++ Optimizer/Direct_Multiple_Shooting_Optimizer.py
@@ -264,6 +264,7 @@ f_zc = ca.interpolant('f_zc','linear',[track_data.arc_s.tolist()],track_data.zc.
 x_opt = np.array(f_xc(s_grid)).flatten() + n_opt * np.array(f_normal1(s_grid)).flatten()
 y_opt = np.array(f_yc(s_grid)).flatten() + n_opt * np.array(f_normal2(s_grid)).flatten()
 z_opt = np.array(f_zc(s_grid)).flatten() + n_opt * np.array(f_normal3(s_grid)).flatten()
+
 #====================================================================================================================================================================================================================
 # Results and Plots 
 ## Racing Line
