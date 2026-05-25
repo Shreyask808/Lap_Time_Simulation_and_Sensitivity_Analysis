fe4d79d16cb0048302c5346e159a870308789c55
Shreyas Kulkarni <shreyask808@gmail.com>
2026-05-25 19:25:47 -0400

    Update tire_models.py

--- Tire Models/tire_models.py
+++ Tire Models/tire_models.py
@@ -28,29 +28,23 @@ def combined_brush_tire_model(u,omega,alpha,Fz,vehicle):
     Cp = vehicle.Cp
     kv = vehicle.kv
     alpha_max = vehicle.alpha_max
-    Fz = ca.fmax(Fz, 50.0)
 
     # Effective Radius Calculation
     r = R - (Fz/kv)
-    a = sqrt(R**2 - r**2 + 1e-6)
-    Pmax = 3*Fz/(4*a*w + 1e-6)
+    a = sqrt(R**2 - r**2)
+    Pmax = 3*Fz/(4*a*w)
 
     # Slip Ratio Definition
     eps = 1e-3
-    alpha = 0.5*(alpha + ca.sqrt(alpha**2 + eps)) - 0.5*((alpha - alpha_max) + ca.sqrt((alpha - alpha_max)**2 + eps))
+    #alpha = alpha_max*ca.tanh((alpha)/(alpha_max))
     kappa = (r*omega - (u))/(u)
     sigma_x = kappa/(1 + kappa)
     sigma_y = tan(alpha)/(1 + kappa)
-    sigma   = ca.sqrt(sigma_x**2 + sigma_y**2 + eps**2)
+    sigma   = ca.sqrt(sigma_x**2 + sigma_y**2)
     
     # Adhesion Length Estimation
     ad_len = 2*a - ((Cp*sigma*a**2)/(mu0*w*Pmax))
-    ad_len = fmax(ad_len, 0)
-    ad_len = fmin(ad_len,2*a)
-
-    ad_len = 0.5*(ad_len + ca.sqrt(ad_len**2 + 1e-6))
-    ad_len = 2*a - 0.5*((2*a - ad_len) + ca.sqrt((2*a - ad_len)**2 + 1e-6))
-    
+    ad_len = 0.5*(ad_len + sqrt(ad_len**2 + 0.01)) - 0.5*(ad_len + sqrt((ad_len - 2*a)**2 + 0.01)) + a
     sliding_term = mu*w*Pmax*((4*ad_len/3) - ((ad_len**2)/a) + ((ad_len**3)/(3*a**2)))
     dir_x   = sigma_x / sigma      # stable because sigma >= sqrt(eps)
     dir_y   = sigma_y / sigma      # same
