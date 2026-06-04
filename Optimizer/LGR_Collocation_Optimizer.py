040bc64c3e555cc082e67fde54fb10876e1cacc3
Shreyas Kulkarni <shreyask808@gmail.com>
2026-06-04 00:42:08 -0400

    Update LGR_Collocation_Optimizer.py

--- Optimizer/LGR_Collocation_Optimizer.py
+++ Optimizer/LGR_Collocation_Optimizer.py
@@ -27,9 +27,51 @@ plt.close('all')
 
 #=======================================================================================================================================================================================================================
 # User Inputs
-h = 50                                                                                                          # Number of Segments the Track is divided into
+h = 60                                                                                                          # Number of Segments the Track is divided into
 p = 5                                                                                                          # Degree of the Polynomial approximating the state in segments
 
+print(f"#================================================================================================================================================================================================================")
+print(f"")
+# Import Track Data
+root = tk.Tk()
+root.withdraw()
+root.attributes('-topmost',True)
+
+file_path1 = filedialog.askopenfilename(title="Select the Track Data",filetypes=[("pickle files","*.pkl"),])
+if file_path1:
+    with open(file_path1, 'rb') as f:  # 'rb' is mandatory for loading pickles
+        track_data = pickle.load(f)
+    print("Loaded Track Data")
+    print(f"Number of Data Points on the Track: {len(track_data.arc_s)}")
+else:
+    raise FileNotFoundError("Track Data file not found")
+
+# Track Segmentation
+segment_length = track_data.arc_s[-1]/h
+print(f"track length in m is: {track_data.arc_s[-1]}")
+print(f"Segment Length in m is: {segment_length}")
+print(f"")
+print(f"#================================================================================================================================================================================================================")
+print(f"#================================================================================================================================================================================================================")
+print(f"")
+# Import Vehicle Data
+root = tk.Tk()
+root.withdraw()
+root.attributes('-topmost',True)
+
+file_path2 = filedialog.askopenfilename(title="Select the Vehicle Data",filetypes=[("json files","*.json")])
+if file_path2:
+    with open(file_path2, 'r') as f:
+        vehicledata = json.load(f)
+        vehicle = SimpleNamespace(**vehicledata)
+    print("Loaded Vehicle Data")
+    print(f"Mass of the Vehicle: {vehicle.m}")
+else:
+    raise FileNotFoundError("Vehicle Data not found")
+print(f"")
+print(f"#================================================================================================================================================================================================================")
+
+#=======================================================================================================================================================================================================================
 #=======================================================================================================================================================================================================================
 # Functions
 ## Flipped Legendre-Gauss-Radau Points
@@ -40,4 +82,9 @@ def flipped_LGR_points(N):
     return col_points
 
 col_p = flipped_LGR_points(p)
-print(f"LGR Points are: {col_p}")
+
+print(f"#================================================================================================================================================================================================================")
+print(f"")
+print(f"Flipped LGR Points used in Analysis are: {col_p}")
+print(f"")
+print(f"#================================================================================================================================================================================================================")
